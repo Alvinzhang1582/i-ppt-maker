9ebc2253c3e3f34d147d9fe5778e9c66e8d02eb3
Alvinzhang1582 <alvinzhangfei@qq.com>
2025-11-19 17:24:33 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,9 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>i-PPT Maker</h1>
-      <p>AI 助力 PPT 生成工具</p>
-      <p>API 地址：<a href="/api/generate">/api/generate</a></p>
-    </div>
-  );
-}
+
